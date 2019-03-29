e28e243e19e0dbeed01e93e4957cab9abd659f62
wnvko <milko.venkov@gmail.com>
2019-03-29 12:04:17 +0200

    fix test browser

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlesCI: {
+      ChromeHeadlessCI: {
         base: "ChromeHeadless",
         flags: [ '-no-sandbox']
       }
