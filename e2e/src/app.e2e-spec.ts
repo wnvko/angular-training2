264b5fd0686bda45199727527add8dfead4da4f4
wnvko <milko.venkov@gmail.com>
2019-03-29 11:03:30 +0200

    Initial commit for project: angular-training2

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { AppPage } from './app.po';
+
+describe('angular-training2 App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display project name in navigation', () => {
+    page.navigateTo();
+    expect(page.getNavText()).toEqual('angular-training2');
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getWelcomeText()).toEqual('Welcome to Ignite UI for Angular!');
+  });
+});
